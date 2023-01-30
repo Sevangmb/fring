65c1caf179ba88655f09ce03c6a4ad3fca89c6ef
Prestalibre <Prestalibre@users.noreply.github.com>
2023-01-30 12:50:34 +0100

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import "./App.css";
 import Carousel from "./components/Carousel";
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Carousel />
-    </div>
-  );
+const App = () => {
+    return (
+      <React.Fragment>
+        <div style={{ maxWidth: 1200, marginLeft: 'auto', marginRight: 'auto', marginTop: 65 }}>
+            <Carousel>
+              <img src="https://via.placeholder.com/1600x800" alt="placeholder" />
+              <img src="https://via.placeholder.com/1600x800" alt="placeholder" />
+              <img src="https://via.placeholder.com/1600x800" alt="placeholder" />
+            </Carousel>
+        </div>
+        <div style={{ maxWidth: 1200, marginLeft: 'auto', marginRight: 'auto', marginTop: 0 }}>
+            <Carousel>
+              <img src="https://via.placeholder.com/1600x800" alt="placeholder" />
+              <img src="https://via.placeholder.com/1600x800" alt="placeholder" />
+              <img src="https://via.placeholder.com/1600x800" alt="placeholder" />
+            </Carousel>
+        </div> 
+      </React.Fragment>       
+    )
 }
 
-export default App;
+export default App
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,16 +1,52 @@
-import React from "react";
-import { Carousel as MUICarousel } from "react-material-ui-carousel";
-import Item from "./Item";
-import slider from "../helper/slider.json";
+import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import '../carousel.css'
 
-function Carousel() {
-  return (
-    <MUICarousel>
-      {slider.map((item) => (
-        <Item key={item.id} item={item} />
-      ))}
-    </MUICarousel>
-  );
+const Carousel = (props) => {
+    const {children} = props
+
+    const [currentIndex, setCurrentIndex] = useState(0)
+const [length, setLength] = useState(children.length)
+
+const next = () => {
+  if (currentIndex < (length - 1)) {
+      setCurrentIndex(prevState => prevState + 1)
+  }
 }
 
-export default Carousel;
+const prev = () => {
+  if (currentIndex > 0) {
+      setCurrentIndex(prevState => prevState - 1)
+  }
+}
+
+// Set the length to match current children from props
+useEffect(() => {
+    setLength(children.length)
+}, [children])
+
+
+    return (
+        <div className="carousel-container">
+            <div className="carousel-wrapper">
+                <button onClick={prev} className="left-arrow">
+                  &lt;
+                </button>
+                <div className="carousel-content-wrapper">
+                <div
+                  className="carousel-content"
+                  style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+                >
+                        {children}
+                    </div>
+                </div>
+                <button onClick={next} className="right-arrow">
+                  &gt;
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Carousel
